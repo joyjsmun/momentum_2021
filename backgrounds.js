37e1536c45faf452cfbfdc705f8e7835cdceba6a
JoyM <joy.js.mun@gmail.com>
2021-08-30 23:46:52 +0900

    ADD: todo.js page begining

---
+++ backgrounds.js
@@ -0,0 +1,8 @@
+const bodyBg = document.querySelector("body");
+const imgs = ["1.jpg","2.jpg","3.jpg"];
+const randomImg = imgs[Math.floor(Math.random()*imgs.length)]
+
+
+bodyBg.style.backgroundImage = "url(' ${randomImg}')";
+
+bodyBg.src = 
