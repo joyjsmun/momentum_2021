4368600cf638363a3632cf81860c7e5b81caf84e
JoyM <joy.js.mun@gmail.com>
2021-09-01 12:50:24 +0900

    FIX : string form problem

--- todo.js
+++ todo.js
@@ -2,29 +2,42 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.querySelector("#todo-list");
 
-let newTodo =""
+let todos =[];
 
 function deleteTodo(event){
  const li = event.target.parentElement;
  li.remove()
 }
 
-function paintTodo(){
+function saveTodo(){
+    localStorage.setItem("todos",JSON.stringify(todos));
+}
+
+function paintTodo(todo){
     const li = document.createElement("li");
     const span = document.createElement("span");
     todoList.appendChild(li);
-    li.innerText = newTodo;
+    li.innerText = todo;
     li.appendChild(span);
     span.innerText = "❌";
     span.addEventListener("click",deleteTodo)
- 
 }
 
 function handleNewTodo(event){
    event.preventDefault();
-   newTodo = todoInput.value;
+   let newTodo = todoInput.value;
+   todos.push(newTodo)
    todoInput.value="";
-   paintTodo();
+   paintTodo(newTodo);
+   saveTodo();
 }
 
-todoForm.addEventListener("submit",handleNewTodo)
+const savedTodos = localStorage.getItem("todos");
+
+if(savedTodos !== null){
+    const parsedTodos = JSON.parse(savedTodos);
+    todos = parsedTodos;
+    parsedTodos.forEach(paintTodo);
+}
+
+todoForm.addEventListener("submit",handleNewTodo);
